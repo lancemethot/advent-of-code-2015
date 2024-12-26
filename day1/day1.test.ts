750fe41e2f00ef585601fc16403daf046a39528f
lancemethot <132783723+lancemethot@users.noreply.github.com>
2024-12-26 17:59:42 -0500

    day 1 part 2

--- day1/day1.test.ts
+++ day1/day1.test.ts
@@ -10,6 +10,19 @@ function partOne(input: string[]): number {
     }, 0);
 }
 
+function partTwo(input: string[]): number {
+    let floor: number = 0;
+    for(let l = 0; l < input.length; l++) {
+        for(let i = 0; i < input[l].length; i++) {
+            floor += (input[l][i] === '(' ? 1 : -1);
+            if(floor === -1) {
+                return ((l + 1) * i) + 1;
+            }
+        }
+    }
+    return 0;
+}
+
 test(day, () => {
 
     expect(partOne(["(())"])).toBe(0);
@@ -26,4 +39,9 @@ test(day, () => {
     expect(partOne([")())())"])).toBe(-3);
 
     expect(partOne(getDayInput(day))).toBe(138);
+
+    expect(partTwo([")"])).toBe(1);
+    expect(partTwo(["()())"])).toBe(5);
+
+    expect(partTwo(getDayInput(day))).toBe(1771);
 });
