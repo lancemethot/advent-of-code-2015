4e9afea79145fc4329bc5cda620ba2787856c16e
lancemethot <132783723+lancemethot@users.noreply.github.com>
2024-12-28 05:17:09 -0500

    day 5 part 2

--- day5/day5.test.ts
+++ day5/day5.test.ts
@@ -17,8 +17,37 @@ function isNice(code: string): boolean {
     return vowelCount > 2 && doublesCount > 0;
 }
 
+function isNice2(code: string): boolean {
+    let pairs: Map<string, number[]> = new Map<string, number[]>();
+    let hasSandwich: boolean = false;
+    let hasPairs: boolean = false;
+    let chars: string[] = code.split('');
+    for(let i = 1; i < chars.length; i++) {
+        let pair: string = `${chars[i - 1]}${chars[i]}`;
+        let prev: string = chars[i - 1 ];
+        let next: string = (i < chars.length - 1) ? chars[i + 1] : '';
+
+        hasSandwich = hasSandwich || (prev === next);
+
+        if(pairs.has(pair)) pairs.get(pair)!.push(i);
+        else pairs.set(pair, [i]);
+    }
+
+    hasPairs = Array.from(pairs).filter(pair => pair[1].length > 1).reduce((acc, pair) => {
+        return acc || pair[1].length > 2 || pair[1].reduce((acc, p, index) => {
+            return acc || (index > 0 ? p - pair[1][index - 1] > 1 : false);
+        }, false);
+    }, false);
+
+    return hasPairs && hasSandwich;
+}
+
 function partOne(input: string[]): number {
     return input.filter(isNice).length;
+}
+
+function partTwo(input: string[]): number {
+    return input.filter(isNice2).length;
 }
 
 test(day, () => {
@@ -29,6 +58,11 @@ test(day, () => {
     expect(isNice("jchzalrnumimnmhp")).toBe(false);
     expect(isNice("haegwjzuvuyypxyu")).toBe(false);
     expect(isNice("dvszwmarrgswjxmb")).toBe(false);
-
     expect(partOne(getDayInput(day))).toBe(236);
+
+    expect(isNice2("qjhvhtzxzqqjkmpb")).toBe(true);
+    expect(isNice2("xxyxx")).toBe(true);
+    expect(isNice2("uurcxstgmygtbstg")).toBe(false);
+    expect(isNice2("ieodomkazucvgmuy")).toBe(false);
+    expect(partTwo(getDayInput(day))).toBe(51);
 });
