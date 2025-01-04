108009b4ba19553254698454929729ef80220deb
lancemethot <132783723+lancemethot@users.noreply.github.com>
2025-01-04 06:32:14 -0500

    day 15 part 2

--- day15/day15.test.ts
+++ day15/day15.test.ts
@@ -25,15 +25,23 @@ function calculateScore(ingredients: Ingredient[], quantities: number[]): number
         acc.durability += ingredient.durability * quantities[index];
         acc.flavor += ingredient.flavor * quantities[index];
         acc.texture += ingredient.texture * quantities[index];
-        acc.calories += ingredient.calories * quantities[index];
         return acc;
     }, { name: 'total', capacity: 0, durability: 0, flavor: 0, texture: 0, calories: 0 });
-    if(total.calories < 0 || total.durability < 0 || total.flavor < 0 || total.texture < 0) return 0;
+    if(total.capacity < 0 || total.durability < 0 || total.flavor < 0 || total.texture < 0) return 0;
     return total.capacity * total.durability * total.flavor * total.texture;
 }
 
-function determineBestCookie(ingredients: Ingredient[], max: number = 100): number {
+function countCalories(ingredients: Ingredient[], quantities: number[]): number {
+    let total: Ingredient = ingredients.reduce((acc, ingredient, index) => {
+        acc.calories += ingredient.calories * quantities[index];
+        return acc;
+    }, { name: 'total', capacity: 0, durability: 0, flavor: 0, texture: 0, calories: 0 });
+    if(total.calories < 0) return 0;
+    return total.calories;
+}
 
+function determineRecipes(ingredients: Ingredient[]): number[][] {
+    const max: number = 100;
     const cookies: number[][] = [];
     const queue: number[][] = [];
 
@@ -44,7 +52,7 @@ function determineBestCookie(ingredients: Ingredient[], max: number = 100): numb
         if(quantities.length < ingredients.length) {
             let remaining: number = max - quantities.reduce((acc, q) => acc + q, 0);
             if(quantities.length < ingredients.length - 1) {
-                for(let i = remaining; i >= 0; i--) {
+                for(let i = remaining; i > (ingredients.length - quantities.length); i--) {
                     queue.push(quantities.concat(i));
                 }
             } else {
@@ -55,11 +63,19 @@ function determineBestCookie(ingredients: Ingredient[], max: number = 100): numb
         }
     }
 
-    return cookies.reduce((acc, cookie) => Math.max(acc, calculateScore(ingredients, cookie)), 0);
+    return cookies;
 }
 
 function partOne(input: string[]): number {
-    return determineBestCookie(parseInput(input));
+    const ingredients: Ingredient[] = parseInput(input);
+    return determineRecipes(parseInput(input)).reduce((acc, recipe) => Math.max(acc, calculateScore(ingredients, recipe)), 0);
+}
+
+function partTwo(input: string[]): number {
+    const ingredients: Ingredient[] = parseInput(input);
+    return determineRecipes(parseInput(input))
+                .filter(recipe => countCalories(ingredients, recipe) === 500)
+                .reduce((acc, recipe) => Math.max(acc, calculateScore(ingredients, recipe)), 0);
 }
 
 test(day, () => {
@@ -67,4 +83,7 @@ test(day, () => {
 
     expect(partOne(getExampleInput(day))).toBe(62842880);
     expect(partOne(getDayInput(day))).toBe(21367368);
+
+    expect(partTwo(getExampleInput(day))).toBe(57600000);
+    expect(partTwo(getDayInput(day))).toBe(1766400);
 });
