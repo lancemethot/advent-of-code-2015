3af234bd4119c673460d769d20596903f2dbd7f6
lancemethot <132783723+lancemethot@users.noreply.github.com>
2025-01-04 07:09:49 -0500

    day 16 part 2

--- day16/day16.test.ts
+++ day16/day16.test.ts
@@ -7,6 +7,18 @@ type Aunt = {
     info: Map<string, number>;
 }
 
+const tape: Map<string, number> = new Map<string, number>();
+tape.set("children", 3);
+tape.set("cats", 7);
+tape.set("samoyeds", 2);
+tape.set("pomeranians", 3);
+tape.set("akitas", 0);
+tape.set("vizslas", 0);
+tape.set("goldfish", 5);
+tape.set("trees", 3);
+tape.set("cars", 2);
+tape.set("perfumes", 1);
+
 function parseInput(input: string[]): Aunt[] {
     return input.reduce((acc, line) => {
         acc.push({
@@ -21,24 +33,27 @@ function parseInput(input: string[]): Aunt[] {
 }
 
 function partOne(input: string[]): number {
+    const matcher = (aunt: Aunt) => Array.from(aunt.info.entries()).reduce((acc, stat) => acc && tape.get(stat[0]) === stat[1], true);
+    return parseInput(input).filter(matcher)[0].id;
+}
 
-    const tape: Map<string, number> = new Map<string, number>();
-    tape.set("children", 3);
-    tape.set("cats", 7);
-    tape.set("samoyeds", 2);
-    tape.set("pomeranians", 3);
-    tape.set("akitas", 0);
-    tape.set("vizslas", 0);
-    tape.set("goldfish", 5);
-    tape.set("trees", 3);
-    tape.set("cars", 2);
-    tape.set("perfumes", 1);
-
-    return parseInput(input).filter(aunt => Array.from(aunt.info.entries()).reduce((acc, stat) => acc && tape.get(stat[0]) === stat[1], true))[0].id;
+function partTwo(input: string[]): number {
+    const matcher = (aunt: Aunt) => Array.from(aunt.info.entries()).reduce((acc, stat) => {
+        if(stat[0] === 'cats' || stat[0] === 'trees') {
+            return acc && tape.get(stat[0])! < stat[1]
+        } else if(stat[0] === 'pomeranians' || stat[0] === 'goldfish') {
+            return acc && tape.get(stat[0])! > stat[1]
+        } else {
+            return acc && tape.get(stat[0]) === stat[1];
+        }
+    }, true);
+    return parseInput(input).filter(matcher)[0].id;
 }
 
 test(day, () => {
     debug(`[**${day}**] ${new Date()}\n\n`, day, false);
 
     expect(partOne(getDayInput(day))).toBe(103);
+    expect(partTwo(getDayInput(day))).toBe(405);
+
 });
