f92cbf04984f55b45da72c5ad5c6d65d89ba4c8b
lancemethot <132783723+lancemethot@users.noreply.github.com>
2025-01-07 18:44:34 -0500

    day 20 part 2

--- day20/day20.test.ts
+++ day20/day20.test.ts
@@ -2,26 +2,39 @@ import { debug } from '../utils';
 
 const day = 'day20';
 
-function sumOfPresents(n: number): number {
-    let sum: number = 0;
-    let d: number = Math.sqrt(n) + 1;
-    for(let i = 1; i <= d; i++) {
-        if(n % i === 0) {
-            sum += i;
-            sum += n / i;
+function partOne(input: number): number {
+    for(let n = 0; n < input; n++) {
+        let sum: number = 0;
+        let d: number = Math.sqrt(n) + 1;
+        for(let i = 1; i <= d; i++) {
+            if(n % i === 0) {
+                sum += i;
+                sum += n / i;
+            }
         }
+        if(sum * 10 >= input) return n;
     }
-    return sum * 10;
+    return input;
 }
 
-function partOne(input: number): number {
-    let i: number = 1;
-    while(sumOfPresents(i++) < input);
-    return i - 1;
+function partTwo(input: number): number {
+    for(let n = 0; n < input; n++) {
+        let sum: number = 0;
+        let d: number = Math.sqrt(n) + 1;
+        for(let i = 1; i <= d; i++) {
+            if(n % i === 0) {
+                if(i <= 50) sum += n / i;
+                if(n/i <= 50) sum += i;
+            }
+        }
+        if(sum * 11 >= input) return n;
+    }
+    return input;
 }
 
 test(day, () => {
     debug(`[**${day}**] ${new Date()}\n\n`, day, false);
 
     expect(partOne(34000000)).toBe(786240);
+    expect(partTwo(34000000)).toBe(831600);
 });
