5a1bb8613fa916f3c13bd60e486ca22520376c85
lancemethot <132783723+lancemethot@users.noreply.github.com>
2025-01-10 05:54:41 -0500

    day 22 part 2

--- day22/day22.test.ts
+++ day22/day22.test.ts
@@ -2,6 +2,14 @@ import { debug, getDayInput, getExampleInput, HeapItem, MinHeap } from '../utils
 
 const day = 'day22';
 
+enum Spells {
+    MISSLE   =  53,
+    DRAIN    =  73,
+    SHIELD   = 113,
+    POISON   = 173,
+    RECHARGE = 229
+}
+
 type Stats = {
     damage: number;
     armor: number;
@@ -39,11 +47,16 @@ function parseInput(input: string[]): Boss {
     }, { hitpoints: 0, damage: 0, armor: 0, poison: 0 });
 }
 
-//                         'missle', 'drain', 'shield', 'poison', 'recharge'
-const spells: number[]  = [      53,      73,      113,      173,        229];
+const spells: number[]  = [
+    Spells.MISSLE,
+    Spells.DRAIN,
+    Spells.SHIELD,
+    Spells.POISON,
+    Spells.RECHARGE
+];
 
-function battle(player: Player, boss: Boss): number {
-    let lowestManaWin: number = Number.MAX_VALUE;
+function battle(player: Player, boss: Boss, hardmode: boolean = false): number {
+    let lowestManaWin: number = Infinity;
     let heap: MinHeap<Battle> = new MinHeap();
     heap.insert({ player: player, boss: boss, effects: [], playerTurn: true, size: 0 });
 
@@ -62,35 +75,35 @@ function battle(player: Player, boss: Boss): number {
         // debug(`- Player has ${p.hitpoints} hit points, ${p.armor} armor, ${p.mana} mana`, day);
         // debug(`- Boss has ${b.hitpoints} hit points`, day);
 
-        // this is the turn loop...
-        // apply any active effects
-        // if playerTurn, pick a spell.. for each available, push onto queue
-        // if boss turn, do damage, set player turn
+        if(hardmode && battle.playerTurn) {
+            p.hitpoints--;
+            if(p.hitpoints <=0) continue;
+        }
 
         // Apply effects
         e.forEach(effect => {
             switch(effect) {
-                case 113: p.shield--;
+                case Spells.SHIELD: p.shield--;
                           // debug(`Shields timer is now ${p.shield}`, day);
                           if(p.shield === 0) {
                             p.armor -= 7;
                             // debug(`Shield wears off, decreasing armor by 7.`, day);
                           }
                     break;
-                case 173: b.hitpoints -= 3;
+                case Spells.POISON: b.hitpoints -= 3;
                           b.poison--;
                           // debug(`Poison deals 3 damage; its timer is now ${b.poison}`, day);
                     break;
-                 case 229: p.mana += 101;
+                 case Spells.RECHARGE: p.mana += 101;
                            p.recharge--;
                            // debug(`Recharge provides 101 mana; its timer is now ${p.recharge}`, day);
             }
         });
 
         // Effects wear off
-        e = e.filter(effect => (effect === 113 && p.shield > 0) ||
-                               (effect === 173 && b.poison > 0) ||
-                               (effect === 229 && p.recharge > 0));
+        e = e.filter(effect => (effect === Spells.SHIELD && p.shield > 0) ||
+                               (effect === Spells.POISON && b.poison > 0) ||
+                               (effect === Spells.RECHARGE && p.recharge > 0));
 
         // Death by poison
         if(b.hitpoints <= 0) {
@@ -99,6 +112,7 @@ function battle(player: Player, boss: Boss): number {
         }
 
         if(battle.playerTurn) {
+            // Player turn.. try all available spells
             spells.filter(spell => !e.includes(spell))
                   .filter(spell => p.mana >= spell)
                   .forEach(spell => {
@@ -109,27 +123,27 @@ function battle(player: Player, boss: Boss): number {
                 let nc = c + spell;
                 np.mana -= spell;
                 switch(spell) {
-                    case 53: 
+                    case Spells.MISSLE: 
                         nb.hitpoints -= 4;
                         // debug(`Player casts Magic Missile, dealing 4 damage.`, day);
                         break;
-                    case 73: 
+                    case Spells.DRAIN: 
                         np.hitpoints += 2;
                         nb.hitpoints -= 2;
                         // debug(`Player casts Drain, dealing 2 damage, and healing 2 hit points.`, day);
                         break;
-                    case 113:
+                    case Spells.SHIELD:
                         np.shield += 6;
                         np.armor += 7;
                         ne.push(spell);
                         // debug(`Player casts Shield, increasing armor by 7.`, day);
                         break;
-                    case 173:
+                    case Spells.POISON:
                         nb.poison += 6;
                         ne.push(spell);
                         // debug(`Player casts Poison.`, day);
                         break;
-                    case 229:
+                    case Spells.RECHARGE:
                         np.recharge += 5;
                         ne.push(spell);
                         // debug(`Player casts Recharge.`, day);
@@ -163,10 +177,20 @@ function partOne(input: string[], playerHitpoints: number, playerManaPoints: num
     return battle(player, boss);
 }
 
+function partTwo(input: string[], playerHitpoints: number, playerManaPoints: number): number {
+    const boss: Boss = parseInput(input);
+    const player: Player = { hitpoints: playerHitpoints, mana: playerManaPoints, damage: 0, armor: 0, shield: 0, recharge: 0 };
+    return battle(player, boss, true);
+}
+
 test(day, () => {
     debug(`[**${day}**] ${new Date()}\n\n`, day, false);
 
     expect(partOne(getExampleInput(day, 1), 10, 250)).toBe(226);
     expect(partOne(getExampleInput(day, 2), 10, 250)).toBe(641);
     expect(partOne(getDayInput(day), 50, 500)).toBe(1269);
+
+    expect(partTwo(getExampleInput(day, 1), 10, 250)).toBe(Infinity);
+    expect(partTwo(getExampleInput(day, 2), 10, 250)).toBe(Infinity);
+    expect(partTwo(getDayInput(day), 50, 500)).toBe(1309);
 });
